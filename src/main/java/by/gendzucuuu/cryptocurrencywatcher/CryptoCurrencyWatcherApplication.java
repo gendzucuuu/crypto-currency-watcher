a85619142b6a5eb91488a4baf04c8630e20b9d1a
gendzucuuu <gendzucuuu@gmail.com>
2022-02-12 20:51:45 +0300

    init commit

---
+++ src/main/java/by/gendzucuuu/cryptocurrencywatcher/CryptoCurrencyWatcherApplication.java
@@ -0,0 +1,13 @@
+package by.gendzucuuu.cryptocurrencywatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoCurrencyWatcherApplication.class, args);
+    }
+
+}
